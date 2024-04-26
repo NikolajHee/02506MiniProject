d6688c8b93ce5da308bfad08a1c791d019d71694
jonathansim <93523007+jonathansim@users.noreply.github.com>
2024-04-26 11:35:06 +0200

    Added data augmentations

--- data_augmentation.py
+++ data_augmentation.py
@@ -4,6 +4,8 @@ This script performs a selection of data augmentation types, e.g. mirroring the 
 
 import numpy as np
 import torch 
+from scipy.ndimage import gaussian_filter, map_coordinates
+
 
 def mirror_input(input, dim: int):
     if not isinstance(dim, int):
@@ -12,21 +14,42 @@ def mirror_input(input, dim: int):
     return torch.flip(input, [dim])
 
 def mirror_horizontal(input):
-    return mirror_input(input, 0)
+    # return mirror_input(input, 0)
+    return np.fliplr(input)
 
 def mirror_vertical(input):
     return mirror_input(input, 1)
 
 
+def elastic_transform(image, alpha, sigma, seed):
+    """    
+    Parameters:
+        image (numpy.ndarray): The image to be transformed.
+        alpha: Scale of the transformation. Controls the intensity of the deformation.
+        sigma: Standard deviation of the Gaussian filter - determines the smoothness of the deformation.
+        random_state (int, optional): Random seed for reproducibility.
+        
+    Returns:
+        numpy.ndarray: The deformed image.
+    """
+    random_state = np.random.RandomState(seed)
+    shape = image.shape
+    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode='constant', cval=0) * alpha
+    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode='constant', cval=0) * alpha
+
+    x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), indexing='ij')
+    indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1))
+
+    distorted_image = map_coordinates(image, indices, order=1).reshape(shape)
+    return distorted_image
 
 
 
 
-# Testing:
-x = torch.arange(8).view(4, 2)
-
-
-# print(x)
-# print(mirror_horizontal(x))
+if __name__ == '__main__':
+    # Testing:
+    x = torch.arange(8).view(4, 2)
+    # print(x)
+    # print(mirror_horizontal(x))
 
 
--- dataset_v2.py
+++ dataset_v2.py
@@ -10,6 +10,8 @@ import torch
 from skimage.transform import resize
 import matplotlib.pyplot as plt
 from data_augmentation import mirror_horizontal, mirror_vertical, elastic_transform
+from scipy.ndimage import gaussian_filter, map_coordinates
+from torchvision import transforms
 # from sklearn.model_selection import train_test_split
 # from sklearn.preprocessing import LabelEncoder
 
@@ -26,59 +28,83 @@ class TRAIN_EM(Dataset):
     Labelled Training Data
         From the 'Segmentation of neuronal structures in EM stacks'
     """
-    def __init__(self, data_path, resize_=None, patch_size=None):
+    def __init__(self, data_path, elastic=False, mirror_h=False, mirror_v=False, total_augment=False):
         data_path = os.path.join(data_path, 'EM_ISBI_Challenge')
-
         self.train_images_path = sorted(glob.glob(os.path.join(data_path, 'train_images', '*.png')))
-
         self.train_labels_path = sorted(glob.glob(os.path.join(data_path, 'train_labels', '*.png')))
 
         assert self.train_images_path != [], \
             "No training images found, please check the path to the training images: " \
             f"{data_path}/train_images/*.png"
 
-        self.resize = resize_
-        self.patch_size = patch_size
+        self.elastic = elastic
+        self.mirror_h = mirror_h
+        self.mirror_v = mirror_v
+        self.total_augment = total_augment
+        self.alpha = 70 #140 before
+        self.sigma = 5
         self.images = []
         self.labels = []
 
-        if self.patch_size is not None:
-            self.prepare_patches()
+        self.prepare_augmentations()
+    
 
-    def prepare_patches(self):
+    def prepare_augmentations(self):
         for img_path, lbl_path in zip(self.train_images_path, self.train_labels_path):
             img = io.imread(img_path)
             lbl = io.imread(lbl_path)
-            if self.resize:
-                img = resize(img, self.resize)
-                lbl = resize(lbl, self.resize)
+            self.images.append(img)
+            self.labels.append(lbl)
+                
+            if self.elastic:
+                seed = np.random.randint(0, 10000)
+                aug_img = elastic_transform(image=img, alpha=self.alpha, sigma=self.sigma, seed=seed)
+                aug_lbl = elastic_transform(image=lbl, alpha=self.alpha, sigma=self.sigma, seed=seed)
 
-            img_patches = self.extract_patches(img)
-            lbl_patches = self.extract_patches(lbl)
-            self.images.extend(img_patches)
-            self.labels.extend(lbl_patches)
+                self.images.append(aug_img)
+                self.labels.append(aug_lbl)
+            
+            if self.mirror_h: 
+                mirrored_img = img[:, ::-1].copy()
+                mirrored_lbl = lbl[:, ::-1].copy()
 
-    def extract_patches(self, image):
-        image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)
-        patches = image.unfold(1, self.patch_size, self.patch_size)
-        patches = patches.unfold(2, self.patch_size, self.patch_size)
-        return patches.contiguous().view(-1, self.patch_size, self.patch_size)
+                self.images.append(mirrored_img)
+                self.labels.append(mirrored_lbl)
+            
+            if self.mirror_v: 
+                mirrored_img = img[::-1, :].copy()
+                mirrored_lbl = lbl[::-1, :].copy()
+
+                self.images.append(mirrored_img)
+                self.labels.append(mirrored_lbl)
+
+            if self.total_augment: 
+                seed = np.random.randint(0, 10000)
+                total_aug_img = elastic_transform(image=img, alpha=self.alpha, sigma=self.sigma, seed=seed)
+                total_aug_lbl = elastic_transform(image=lbl, alpha=self.alpha, sigma=self.sigma, seed=seed)
+                total_aug_img = total_aug_img[::-1, ::-1].copy()
+                total_aug_lbl = total_aug_lbl[::-1, ::-1].copy()
+
+                self.images.append(total_aug_img)
+                self.labels.append(total_aug_lbl)
+
 
     def __len__(self):
-        if self.patch_size: 
-            return len(self.images)
-        else:   
-            return len(self.train_images_path)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        if self.patch_size is not None:
-            img = self.images[idx]
-            lbl = self.labels[idx]
-            return (img.view(1,self.patch_size,self.patch_size) / 255, lbl.view(1,self.patch_size,self.patch_size) / 255)
-        else:
-            return (torch.tensor(io.imread(self.train_images_path[idx])).view(1, 512, 512)/255, 
-                    torch.tensor(io.imread(self.train_labels_path[idx])).view(1, 512, 512)/255)
         
+        img = self.images[idx]
+        lbl = self.labels[idx]
+
+        # Normalize and convert to tensor
+        img = torch.tensor(img, dtype=torch.float32).unsqueeze(0) / 255
+        lbl = torch.tensor(lbl, dtype=torch.float32).unsqueeze(0) / 255
+        
+        return (img, lbl)
+
+        # return (img.view(1,self.patch_size,self.patch_size) / 255, lbl.view(1,self.patch_size,self.patch_size) / 255)
+
 class TEST_EM(Dataset):
     """
     Unlabelled Test Data
@@ -132,7 +158,7 @@ class TEST_EM(Dataset):
         
 if __name__ == '__main__':
 
-    train_dataset = TRAIN_EM('', patch_size=128)
+    train_dataset = TRAIN_EM('', elastic=False, mirror_v=False, total_augment=True)
 
     # for training in batches:
     train_loader = DataLoader(dataset=train_dataset,
@@ -159,5 +185,8 @@ if __name__ == '__main__':
 
         break
     
+
+    plt.imshow(train_dataset[1][1].squeeze())
+    # plt.imshow(train_dataset[1][1].squeeze())
     #print(f"max: {np.max(train_dataset[i][0])}, min: {np.min(train_dataset[i][0])}")
 
